ff8f117f4487d5f1f9f67ff3723723a6ace443b4
Katarina16004 <veljkovickatarina56@gmail.com>
2025-03-24 12:06:10 +0100

    dizajn login prozora

--- HotelManagementSystem/MainWindow.xaml.cs
+++ HotelManagementSystem/MainWindow.xaml.cs
@@ -16,9 +16,43 @@ namespace HotelManagementSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool vidljivostLozinke = false;
         public MainWindow()
         {
             InitializeComponent();
+            PasswordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            EyeIcon.Visibility = Visibility.Collapsed;
+            UsernameTextBox.Focus();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+                EyeIcon.Visibility = Visibility.Collapsed;
+            else
+                EyeIcon.Visibility = Visibility.Visible;
+        }
+
+        private void EyeIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (vidljivostLozinke)
+            {
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordBox.Password = PasswordTextBox.Text;
+
+                EyeIcon.Source = new BitmapImage(new System.Uri("Resources/otvoreno_oko.png", System.UriKind.Relative));
+            }
+            else
+            {
+                PasswordTextBox.Visibility = Visibility.Visible;
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordTextBox.Text = PasswordBox.Password;
+
+                EyeIcon.Source = new BitmapImage(new System.Uri("Resources/zatvoreno_oko.png", System.UriKind.Relative));
+            }
+
+            vidljivostLozinke = !vidljivostLozinke;
         }
     }
 }
